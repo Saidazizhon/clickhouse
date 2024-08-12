3fe973997e0b8555416d57f0e04b540d3b699ac8
Saidazizhon <asrarovs26@gmail.com>
2024-08-12 10:51:01 +0500

    tugadi

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Banner from './components/Banner';
-import Category from './components/Category';
-import Products from './components/Products';
-import Footer from './components/Footer';
-import axios from 'axios';
+import { useState } from 'react'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Kategorey from './components/kategorey/Kategorey'
+import Json from './components/json/Json'
+import Footer from './components/footer/Footer'
+
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [categories, setCategories] = useState([]);
-  const [discountedProducts, setDiscountedProducts] = useState([]);
-  const [availableProducts, setAvailableProducts] = useState([]);
 
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [isDarkMode]);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
-
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const response = await axios.get('https://dummyjson.com/products/categories');
-      setCategories(response.data);
-    };
-
-    const fetchProducts = async () => {
-      const response = await axios.get('https://dummyjson.com/products');
-      const products = response.data.products;
-
-      const discounted = products.filter(product => product.discountPercentage > 0);
-      setDiscountedProducts(discounted);
-
-      setAvailableProducts(products);
-    };
-
-    fetchCategories();
-    fetchProducts();
-  }, []);
 
   return (
-    <div className={isDarkMode ? 'dark' : ''}>
-      <Navbar toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
-      <Banner />
-      <Category categories={categories} />
-      <Products products={discountedProducts} title="Скидки %" />
-      <Products products={availableProducts} title="Товары в наличии" />
-      <Footer />
-    </div>
-  );
+    <>
+      <Header />
+      <Hero />
+      <Kategorey/>
+      <Json/>
+      <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class', // Dark Mode'ni qo'llab-quvvatlash uchun class ni qo'shamiz
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
